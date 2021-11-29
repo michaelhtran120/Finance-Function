d245e2e972c0ef9112884a041249653db340852f
Michael Tran <michaelhtran120@gmail.com>
2021-11-29 07:21:29 -0800

    link activation on scroll implemented

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,6 +12,9 @@ navLinks.forEach((link) => {
 
 const introSection = document.getElementById("introduction");
 const definitionSection = document.getElementById("definition");
+const frameworkSection = document.getElementById("framework");
+const processSection = document.getElementById("process");
+const applicationSection = document.getElementById("application");
 
 console.log(introSection);
 
@@ -23,9 +26,23 @@ window.addEventListener("scroll", () => {
         navLinks[0].classList.add("active");
         const oLinks = navLinks.filter((oLink) => oLink !== navLinks[0]);
         oLinks.forEach((oLink) => oLink.classList.remove("active"));
-    } else if (window.scrollY > definitionSection.offsetTop - 100 && window.scrollY < definitionSection.offsetTop + definitionSection.scrollHeight) {
+    } else if (
+        window.scrollY > definitionSection.offsetTop - 100 &&
+        window.scrollY < definitionSection.offsetTop + definitionSection.scrollHeight - 100
+    ) {
         navLinks[1].classList.add("active");
         const oLinks = navLinks.filter((oLink) => oLink !== navLinks[1]);
+        oLinks.forEach((oLink) => oLink.classList.remove("active"));
+    } else if (
+        window.scrollY > frameworkSection.offsetTop - 200 &&
+        window.scrollY < frameworkSection.offsetTop + frameworkSection.scrollHeight - 200
+    ) {
+        navLinks[2].classList.add("active");
+        const oLinks = navLinks.filter((oLink) => oLink !== navLinks[2]);
+        oLinks.forEach((oLink) => oLink.classList.remove("active"));
+    } else if (window.scrollY > processSection.offsetTop - 250 && window.scrollY < processSection.offsetTop + processSection.scrollHeight) {
+        navLinks[3].classList.add("active");
+        const oLinks = navLinks.filter((oLink) => oLink !== navLinks[3]);
         oLinks.forEach((oLink) => oLink.classList.remove("active"));
     }
 });
